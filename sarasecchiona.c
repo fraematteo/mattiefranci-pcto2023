8270ce62753ca54565cc365f85c2136e8c6f99bd
fraematteo <124669321+fraematteo@users.noreply.github.com>
2023-02-07 15:36:09 +0100

    Add files via upload

--- sarasecchiona.c
+++ sarasecchiona.c
@@ -5,17 +5,17 @@ int main()
 int a;
 int b;
 
-printf("sono multipli? \n");
+printf("inserisci due numeri, sono multipli? \n");
 scanf("%d %d", &a , &b);
 int risultato= a%b; 
 
-if(risultato = 0)
+if(risultato == 0)
 {
-    printf("multiplo");
+    printf("multiplo\n");
 }
 else
 {
-    printf("a è non multiplo di b");
+    printf("a è non multiplo di b\n");
 }
 
 
