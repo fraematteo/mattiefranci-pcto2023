7497aeb884ed58cbe150410331677993aabc5d3b
fraematteo <124669321+fraematteo@users.noreply.github.com>
2023-02-08 11:42:56 +0100

    Add files via upload

---
+++ lunaesole.c
@@ -0,0 +1,34 @@
+#include  <stdio.h>
+
+int main()
+{
+
+    int anno;
+
+    printf("quando sei nato? \n");
+    scanf ("%d",&anno);
+
+    if(anno > 1969 )
+    {
+        printf ("annodopo: %d\n", anno-1969);
+    }
+    else if( anno<1969)
+    {
+        printf("annoprima: %d\n", 1969-anno);
+
+    }   
+    else 
+    {
+        printf("stessoanno\n");
+    }
+
+
+
+
+
+
+
+
+
+
+}
